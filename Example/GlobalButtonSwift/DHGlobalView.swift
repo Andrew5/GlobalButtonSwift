ac2e3fdbf8ea57619368ad37676b1d41c58c3c63
Mr. Zhao <localhost3585@gmail.com>
2021-09-18 15:10:29 +0800

    设置环境变量全局悬浮按钮

---
+++ Example/GlobalButtonSwift/DHGlobalView.swift
@@ -0,0 +1,9 @@
+//
+//  DHGlobalView.swift
+//  GlobalButtonSwift_Example
+//
+//  Created by jabraknight on 2021/9/14.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import Foundation
