045ed0e22ecad824b08ee951c9c338bd4f37e167
jabraknight <localhost3585@gmail.com>
2024-07-05 20:40:54 +0800

    feat: 增加数据清理功能

---
+++ GlobalButtonSwift/Classes/DHGlobeAppManager.swift
@@ -0,0 +1,33 @@
+//
+//  DHGlobeAppManager.swift
+//  GlobalButtonSwift
+//
+//  Created by rillakkuma on 7/5/24.
+//
+
+import Foundation
+import UIKit
+
+class DHGlobeAppManager {
+    
+    // 清理本地数据
+    static func clearLocalData() {
+        UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+        UserDefaults.standard.synchronize()
+    }
+    
+    // 模拟应用重启
+    static func restartApp() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            if let window = UIApplication.shared.windows.first {
+                UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: {
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let initialViewController = storyboard.instantiateInitialViewController()
+                    window.rootViewController = initialViewController
+                }) { completed in
+                    exit(0)
+                }
+            }
+        }
+    }
+}
